a0aaaaeb189857d71e18aeae45ff8644aad8f614
CHiepTRAN <107661128+HHiepz@users.noreply.github.com>
2024-12-25 01:41:04 +0700

    Change "priority" (varchar to tinyint)

--- database/migrations/2024_12_24_182810_create_skill_categories_table.php
+++ database/migrations/2024_12_24_182810_create_skill_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('skill_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('priority');
+            $table->tinyInteger('priority');
             $table->timestamps();
         });
     }
